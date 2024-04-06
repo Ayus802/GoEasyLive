e07e745ab1879b47fa5993c852a56c7b9aa689b9
Ayush Kumar Gupta <guptayush47@gmail.com>
2024-04-06 18:49:36 +0530

    Frontend 1st

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import { Route, Routes } from 'react-router-dom';
+import Registration from './components/Registration';
+import Login from './components/Login';
+import Main from './components/Main';
+import { useState } from 'react';
+import Flat from './components/flat';
+import WatchList from './components/WatchList';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [verified , setVerified] = useState(false)
+  const [ids, setIds] = useState(0)
 
   return (
-    <>
-    
-    </>
-  )
+    <div className="text-center bg-red-100">
+        <Navbar verified={verified} setVerified={setVerified}/>
+      <Routes>
+        <Route path='/Login' element={<Login verified={verified} setVerified={setVerified}/>} />
+        <Route path='/Registration' element={<Registration/>} />
+        <Route path='/' element={<Hero />} />
+        <Route path='/Main' element={<Main ids={ids} setIds={setIds}/>} />
+        <Route path='/Main/flat' element={<Flat id={ids}/>}/>
+        <Route path='/watchList' element={<WatchList/>}/>
+      </Routes>
+
+    </div>
+  );
 }
 
-export default App
+export default App;
